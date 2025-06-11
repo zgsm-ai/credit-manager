d008671d25e66d2f5ff98e6e9fd275991a001af9
xjm <2413294257@qq.com>
2025-06-11 15:29:15 +0800

    feat: project init

---
+++ src/locales/index.ts
@@ -0,0 +1,15 @@
+import { createI18n } from 'vue-i18n'
+import zh from './zh.json'
+import en from './en.json'
+
+const i18n = createI18n({
+    legacy: false,
+    locale: 'zh',
+    fallbackLocale: 'en', 
+    messages: {
+        zh,
+        en,
+    },
+})
+
+export default i18n
